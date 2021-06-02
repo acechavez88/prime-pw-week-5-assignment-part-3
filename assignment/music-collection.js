fa23961c3a534973d1c2c6c59d22e262c14119be
Abram Chavez <abram.chavez820@gmail.com>
2021-06-02 15:12:06 -0500

    redoing function and console log to made 6 albums

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -2,36 +2,21 @@ let collection = [];
 function addToCollection(title, artist, yearPublished){
   return true
 }
-const texaaasLive = {
+const texaasLive = {
   title : 'boot scoot boogie',// created pseudo music info to test.
   artist:' Texas man',
   yearPublished: 2012,
 };
-collection.push(texaaasLive);// attempted to push object to array..
+collection.push(texaasLive);// attempted to push object to array..
 console.log(collection);// console log collection and object was displayed..
+// didnt understand instructions.. will test function and add 6 albums to
+//funciton below..
+console.log(addToCollection('you me and her and her','Texas Man', 2012));
+console.log(addToCollection('I cant not stop lovin You','Texas Man', 2012));
+console.log(addToCollection('Sammy is dead','Dude, your a girl', 2012));
+console.log(addToCollection('Drink my sorrows','Red man group', 2020));
+console.log(addToCollection('What them hands do','Dillary Huff', 2006));
+console.log(addToCollection('Say it to my face','Cher', 2012));
 
-// attempting to create 4 objects to represent 6 albums per instructions..
-const godmadedirt = {
-  title: 'You and me and her and her',
-  artist:' Texas man ft. shakira',
-  yearPublished: 2012,
-};
-const shedidmedirty = {
-  title : ' I cant not look at you',// created pseudo music info to test.
-  artist: 'bert and ernie live',
-  yearPublished: 2018
-};
-
-const boyeatsworld = {
-  title : 'boot scoot boogie',// created pseudo music info to test.
-  artist: 'Dude, your a Girl',
-  yearPublished: 2020,
-  album: '1+1 = Love',
-};
-collection.push(godmadedirt); //attempting to push object to array..
+collection.push(album1, album2, album3, album4, album5);
 console.log(collection);
-collection.push(shedidmedirty); //attempting to push object to array..
-console.log(collection);
-collection.push(boyeatsworld); //attempting to push object to array..
-console.log(collection);
-// console log reads [{...}].. with list of artists when expanded..
