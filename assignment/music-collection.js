b4e366cb58fb3e0b0334c54aa0ae2bbd24d65ee1
Abram Chavez <abram.chavez820@gmail.com>
2021-06-01 23:21:01 -0500

    commit comments and console log result

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -3,8 +3,9 @@ function addToCollection(title, artist, yearPublished){
   return true
 }
 const music = {
-  title : 'boot scoot boogie',
+  title : 'boot scoot boogie',// created pseudo music info to test.  
   artist:' Texas man',
   yearPublished: 2012,
 }
-console.log(collection);
+collection.push(music);// attempted to push object to array..
+console.log(collection);// console log collection and object was displayed..
