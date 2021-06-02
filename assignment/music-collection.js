460d6f824c1b0cad454d0e5449faf77f404830bb
Abram Chavez <abram.chavez820@gmail.com>
2021-06-02 13:35:36 -0500

    making multiple objects for albums..

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -2,10 +2,29 @@ let collection = [];
 function addToCollection(title, artist, yearPublished){
   return true
 }
-const music = {
-  title : 'boot scoot boogie',// created pseudo music info to test.  
+const texaaasLive = {
+  title : 'boot scoot boogie',// created pseudo music info to test.
   artist:' Texas man',
   yearPublished: 2012,
-}
-collection.push(music);// attempted to push object to array..
+};
+collection.push(texaaasLive);// attempted to push object to array..
 console.log(collection);// console log collection and object was displayed..
+
+// attempting to create 6 objects to represent 6 albums per instructions..
+const album = {
+  title: 'You and me and her and her',
+  artist:' Texas man',
+  yearPublished: 2012,
+};
+const Shedidmedirty = {
+  title : ' I cant not look at you',// created pseudo music info to test.
+  artist: 'bert and ernie live',
+  yearPublished: 2018
+};
+
+const music = {
+  title : 'boot scoot boogie',// created pseudo music info to test.
+  artist: 'Dude, your a Girl',
+  yearPublished: 2020,
+  album: '1+1 = Love',
+};
