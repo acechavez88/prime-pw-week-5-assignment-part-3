ff683ac284d075c8e6efc269b3fe9bc21cc2362b
Abram Chavez <abram.chavez820@gmail.com>
2021-06-02 13:39:18 -0500

    updating ogject names for testing

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -10,19 +10,19 @@ const texaaasLive = {
 collection.push(texaaasLive);// attempted to push object to array..
 console.log(collection);// console log collection and object was displayed..
 
-// attempting to create 6 objects to represent 6 albums per instructions..
-const album = {
+// attempting to create 4 objects to represent 6 albums per instructions..
+const godmadedirt = {
   title: 'You and me and her and her',
-  artist:' Texas man',
+  artist:' Texas man ft. shakira',
   yearPublished: 2012,
 };
-const Shedidmedirty = {
+const shedidmedirty = {
   title : ' I cant not look at you',// created pseudo music info to test.
   artist: 'bert and ernie live',
   yearPublished: 2018
 };
 
-const music = {
+const boyeatsworld = {
   title : 'boot scoot boogie',// created pseudo music info to test.
   artist: 'Dude, your a Girl',
   yearPublished: 2020,
