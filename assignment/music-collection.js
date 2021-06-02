35b86c785b74af15af702a2e257defdeaa3f8e39
Abram Chavez <abram.chavez820@gmail.com>
2021-06-02 15:20:44 -0500

    attempted to console but console read initialization..

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -18,5 +18,38 @@ console.log(addToCollection('Drink my sorrows','Red man group', 2020));
 console.log(addToCollection('What them hands do','Dillary Huff', 2006));
 console.log(addToCollection('Say it to my face','Cher', 2012));
 
-collection.push(album1, album2, album3, album4, album5);
+collection.push(godmadedirt, shedidmedirty, boyeatsworld, sayitdontsprayit, litterbug,stonecoldsaidso);
 console.log(collection);
+
+const godmadedirt = {
+  title: 'You and me and her and her',
+  artist:' Texas man',
+  yearPublished: 2012,
+};
+
+const shedidmedirty = {
+  title : ' I cant not stop lovin You',// created pseudo music info to test.
+  artist: 'Texas Man',
+  yearPublished: 2012,
+};
+
+const boyeatsworld = {
+  title : 'Sammy is dead',// created pseudo music info to test.
+  artist: 'Dude, your a girl',
+  yearPublished: 2012,
+};
+const sayitdontsprayit = {
+  title : 'Drink my sorrows',// created pseudo music info to test.
+  artist: 'Red man group',
+  yearPublished: 2020,
+};
+const litterbug = {
+  title : 'What them hands do',// created pseudo music info to test.
+  artist: 'Dillary Huff',
+  yearPublished: 2006,
+};
+const stonecoldsaidso = {
+  title : 'Say it to my face',// created pseudo music info to test.
+  artist: 'Cher',
+  yearPublished: 2012,
+};
