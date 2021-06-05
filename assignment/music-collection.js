f4ce564eafe8ddb0c6d3219ec661d407262a09a9
Abram Chavez <abram.chavez820@gmail.com>
2021-06-05 18:14:37 -0500

    completed showcollection function and addtocollection function with test

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -1,58 +1,39 @@
-let collection = [];
+const collection = [];
 function addToCollection(title, artist, yearPublished){
-  return `${title + artist + yearPublished}`; // should return with title, art.,year..
-};
-
-const texaasLive = {
-  title : 'boot scoot boogie',// created pseudo music info to test.
-  artist:' Texas man',
-  yearPublished: 2012,
-};
-
-const godmadedirt = {
-  title: 'You and me and her and her',
-  artist:' Texas man',
-  yearPublished: 2012,
-};
-
-const shedidmedirty = {
-  title : ' I cant not stop lovin You',// created pseudo music info to test.
-  artist: 'Texas Man',
-  yearPublished: 2012,
-};
-
-const boyeatsworld = {
-  title : 'Sammy is dead',// created pseudo music info to test.
-  artist: 'Dude, your a girl',
-  yearPublished: 2012,
-};
-const sayitdontsprayit = {
-  title : 'Drink my sorrows',// created pseudo music info to test.
-  artist: 'Red man group',
-  yearPublished: 2020,
-};
-const litterbug = {
-  title : 'What them hands do',// created pseudo music info to test.
-  artist: 'Dillary Huff',
-  yearPublished: 2006,
-};
-const stonecoldsaidso = {
-  title : 'Say it to my face',// created pseudo music info to test.
-  artist: 'Cher',
-  yearPublished: 2012,
-};
-
-collection.push(texaasLive, godmadedirt, shedidmedirty, boyeatsworld, sayitdontsprayit, litterbug,stonecoldsaidso);
-for (props in texaasLive,godmadedirt,shedidmedirty,boyeatsworld,sayitdontsprayit,litterbug,stonecoldsaidso){
-  console.log(texaasLive,godmadedirt,shedidmedirty,boyeatsworld,sayitdontsprayit,litterbug,stonecoldsaidso[props]);
-}// console log the properties in all 7 objects...
-console.log(collection.length);// console log length of array
-
-function showCollection(){
-
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+  return album; // should return with title, art.,year..
 }
+
+console.log(addToCollection('texaasLive','bart scott','2010'));
+console.log(addToCollection('tex','eddie V','2001'));
+console.log(addToCollection('txLive','that guy joe','2002'));
+console.log(addToCollection('texasLive','this guy','2012'));
+console.log(addToCollection('live','gabroniee','2006'));
+console.log(addToCollection('texas','the what','2012'));
+
+
+function showCollection(array){
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++)
+    console.log(array[i].title,'by',array[i].artist,'published in', array[i].yearPublished);
+    return true
+  }
+console.log(showCollection(collection));
+
 
 function findByArtist(artist){
-  let array = []
-
+  let anything = ['texaasLive','bart scott','2010']
+  for (var i = 0; i < collection.length; i++) {
+    if (collection.length == anything.length) {
+      anything.push(artist)
+      return true
+    }
+  }
 }
+console.log(findByArtist('texaasLive','bart scott','2010'));
+console.log(findByArtist(''));
