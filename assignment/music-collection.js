51eb1eed09fbacf4fa5a2deaa3b72b6e96941b6a
Abram Chavez <abram.chavez820@gmail.com>
2021-06-05 23:13:38 -0500

    commit with tests comments and working console log and html question..

--- assignment/music-collection.js
+++ assignment/music-collection.js
@@ -1,22 +1,25 @@
 const collection = [];
 function addToCollection(title, artist, yearPublished){
+  //making album object with parameters..
   let album = {
     title: title,
     artist: artist,
     yearPublished: yearPublished
   }
-  collection.push(album);
-  return album; // should return with title, art.,year..
+  collection.push(album); //pushing album to collection array..
+  return album;
 }
 
+console.log("****** addToCollection Test*******");
 console.log(addToCollection('texaasLive','bart scott','2010'));
 console.log(addToCollection('tex','eddie V','2001'));
-console.log(addToCollection('txLive','that guy joe','2002'));
+console.log(addToCollection('txLive','bart scott','2002')); // changed artist to test..
 console.log(addToCollection('texasLive','this guy','2012'));
 console.log(addToCollection('live','gabroniee','2006'));
 console.log(addToCollection('texas','the what','2012'));
 
 
+console.log("****** showCollection Test*******");
 function showCollection(array){
   console.log(array.length);
   for (let i = 0; i < array.length; i++)
@@ -27,13 +30,14 @@ console.log(showCollection(collection));
 
 
 function findByArtist(artist){
-  let anything = ['texaasLive','bart scott','2010']
-  for (var i = 0; i < collection.length; i++) {
-    if (collection.length == anything.length) {
-      anything.push(artist)
-      return true
+  let anything = [] //empty array..
+  for (let thisAlbum of collection) { //for of loop properties of collection..
+    if (thisAlbum.artist === artist) {//testing for matching artists in collection..
+      anything.push(thisAlbum);// will push any matching albums to anything array..
     }
   }
+  return anything; // function to return anything after loop completes..
 }
-console.log(findByArtist('texaasLive','bart scott','2010'));
-console.log(findByArtist(''));
+console.log("****** findByArtist Test*******");
+console.log(findByArtist('bart scott'));//test should output (2) array..
+console.log(findByArtist());// test should return empty []..
